ef4f09e29b0a8cc96c71d7f5a5a8483ad5715dc7
siwoon1602 <siwoon1602@gmail.com>
2025-07-14 01:06:42 +0900

    리액트쿼리 셋팅

--- shop/src/index.js
+++ shop/src/index.js
@@ -5,13 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
